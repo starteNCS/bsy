2f6367851339662df890800cae92cf6cfe8d9ac0
Philipp <startenyt@gmail.com>
2021-04-26 14:27:42 +0200

    leider haben wir den rs tree noch nicht hin bekommen, lenni macht das dann am wochenende

--- 2/tree/tree.py
+++ 2/tree/tree.py
@@ -7,6 +7,7 @@ class TreeNodeColor(Enum):
 class TreeNode:
     left: 'TreeNode' = None
     right: 'TreeNode' = None
+    parent: 'TreeNode' = None
     key: int
     value: int
     color: TreeNodeColor
@@ -17,34 +18,72 @@ class TreeNode:
     def get_right(self):
         return self.right
 
+count = 0
+
 def insert_into_tree(current_node: TreeNode, key: int, value: int) -> TreeNode:
+    global count
     if current_node == None:
         current_node = TreeNode()
         current_node.key = key
         current_node.value = value
+        current_node.color = TreeNodeColor.red if count > 0 else TreeNodeColor.black
+        count += 1
         return current_node
 
     if current_node.key < key:
         current_node.right = insert_into_tree(current_node.get_right(), key, value)
+        current_node.right.parent = current_node
 
     if current_node.key > key:
         current_node.left = insert_into_tree(current_node.get_left(), key, value)
+        current_node.left.parent = current_node
+
+    if current_node.color == TreeNodeColor.red:
+        current_node.color = TreeNodeColor.black
+
+        #right_blacks = count_blacks(current_node.right)
+        #left_blacks = count_blacks(current_node.left)
+        #if left_blacks - right_blacks < -1:
+
+        #if right_blacks - left_blacks < -1:
+
 
     return current_node
 
-def print_inorder(current_node: TreeNode):
+def rotate_left(pivot: TreeNode):
+
+
+def count_blacks(current_node: TreeNode) -> int:
+    black_count = 0
+    if current_node == None:
+        return 0
+
+    black_count += count_blacks(current_node.get_left())
+    black_count += 1 if current_node.color == TreeNodeColor.black else 0
+    black_count += count_blacks(current_node.get_right())
+    return black_count
+
+def print_inorder(current_node: TreeNode, test):
     if current_node == None:
         return
 
-    print_inorder(current_node.get_left())
-    print(current_node.key)
-    print_inorder(current_node.get_right())
+    print_inorder(current_node.get_left(), test + "-")
+    print(test, current_node.key, current_node.parent.key if current_node.parent is not None else -1)
+    print_inorder(current_node.get_right(), test + "-")
 
 root = None
-root = insert_into_tree(root, 0, 1)
-root = insert_into_tree(root, 2, 1)
-root = insert_into_tree(root, 3, 1)
-root = insert_into_tree(root, 5, 1)
-root = insert_into_tree(root, 69, 1)
+root = insert_into_tree(root, 35, 1)
+root = insert_into_tree(root, 9, 1)
+root = insert_into_tree(root, 50, 1)
+root = insert_into_tree(root, 57, 1)
+root = insert_into_tree(root, 68, 1)
+root = insert_into_tree(root, 43, 1)
+root = insert_into_tree(root, 46, 1)
 
-print_inorder(root)
+
+print_inorder(root, "")
+print()
+print("asdfgsadgfsadfasdfsadfasdfasddfsadfasdfasdfasfdasdf")
+print()
+rotate_left(root.right.left)
+print_inorder(root, "")
--- 2/warteraum-rbtree/warteraum.py
+++ 2/warteraum-rbtree/warteraum.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import bintrees.rbtree as rbtree
+import time
 
 ankunft_tree = rbtree.RBTree()
 lmb = 0.1
 mue = 0.2
-nmax = 1000000
+nmax = 20000
 zwischenankunft = np.random.exponential(1. / lmb, nmax)
 
 ankunft = np.cumsum(zwischenankunft)
@@ -13,28 +14,33 @@ bearbeitungszeit = np.random.exponential(1. / mue, nmax)
 for z in zip(ankunft, bearbeitungszeit):
     ankunft_tree.insert(z[0], z[1])
 
+tic = time.perf_counter()
 start = []
 end = []
 latest = 0.
 
 
 def do_work(key: float, value: float):
+    global latest
     if latest < key:
         start.append(key)
         end.append(key + value)
-        last = latest + value
+        latest = latest + value
     else:
         start.append(latest)
         end.append(latest + value)
-        last = latest + value
+        latest = latest + value
 
 
 while ankunft_tree.count > 0:
-    current_item = ankunft_tree.min_item()
+    current_item = ankunft_tree.pop_min()
     do_work(current_item[0], current_item[1])
-    ankunft_tree.remove(current_item[0])
+
 
 start = np.array(start)
 end = np.array(end)
+toc = time.perf_counter()
+
 plt.hist(end-ankunft,bins=100)
 plt.show()
+print(f"Took {toc - tic:0.4f}")
