7cbad0f2126c2637f6473a53bcc0be41ba04289b
Philipp <startenyt@gmail.com>
2021-05-01 21:51:24 +0200

    warteraum mit threading

--- 2/warteraum-rbtree/warteraum.py
+++ 2/warteraum-rbtree/warteraum.py
@@ -2,11 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import bintrees.rbtree as rbtree
 import time
+import threading
+import concurrent.futures
 
 ankunft_tree = rbtree.RBTree()
 lmb = 0.1
 mue = 0.2
 nmax = 20000
+nmaxthreads = nmax / 2
+
 zwischenankunft = np.random.exponential(1. / lmb, nmax)
 
 ankunft = np.cumsum(zwischenankunft)
@@ -19,9 +23,9 @@ start = []
 end = []
 latest = 0.
 
-
 def do_work(key: float, value: float):
     global latest
+    time.sleep(value)
     if latest < key:
         start.append(key)
         end.append(key + value)
@@ -32,15 +36,22 @@ def do_work(key: float, value: float):
         latest = latest + value
 
 
-while ankunft_tree.count > 0:
+def process_task():
     current_item = ankunft_tree.pop_min()
     do_work(current_item[0], current_item[1])
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=nmaxthreads) as executor:
+    while ankunft_tree.count > 0:
+        future = executor.submit(process_task)
+    # do_work(current_item[0], current_item[1])
 
 
 start = np.array(start)
 end = np.array(end)
 toc = time.perf_counter()
 
-plt.hist(end-ankunft,bins=100)
+print(f"Took {toc - tic:0.4f}")
+
+plt.hist(end - ankunft, bins=100)
 plt.show()
 print(f"Took {toc - tic:0.4f}")
